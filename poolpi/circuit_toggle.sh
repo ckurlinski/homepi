355275b8dd55723105765f1734e5f36767c177b2
ckurlinski <ckurlinski@me.com>
2017-07-06 11:00:56 -0400

    Spelling Erors

--- poolpi/circuit_toggle.sh
+++ poolpi/circuit_toggle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-circuit_toogle_fn () {
+circuit_toggle_fn () {
   circuit_select_fn
   _status=(`node -pe 'JSON.parse(process.argv[1]).status' "$(curl -s ${_server_ip}:${_server_port}/circuit/${_circuit})"`)
   if [[ ${_status} == 0 ]]; then
