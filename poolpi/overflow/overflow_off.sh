0094177be7b5b9aab48fc3f541057d36408bd052
ckurlinski <ckurlinski@me.com>
2017-07-06 16:05:00 -0400

    Update cmd line

--- poolpi/overflow/overflow_off.sh
+++ poolpi/overflow/overflow_off.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 overflow_off_cmd_fn() {
-  curl -s 127.0.0.1:3000/circuit/2/toggle
+  curl -s 127.0.0.1:3000/circuit/3/toggle > /dev/null
 }
 overflow_off_cmd_fn
--- poolpi/vacuum/vac_off.sh
+++ poolpi/vacuum/vac_off.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 vac_off_cmd_fn() {
-  curl -s 127.0.0.1:3000/circuit/2/toggle
+  curl -s 127.0.0.1:3000/circuit/2/toggle > /dev/null
 }
 vac_off_cmd_fn
