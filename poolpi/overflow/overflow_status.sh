ed31267223fc317a2dd4e7e1a61bd79bb95e0acd
ckurlinski <ckurlinski@me.com>
2017-07-06 16:51:52 -0400

    Testing Homekit status query

--- poolpi/overflow/overflow_status.sh
+++ poolpi/overflow/overflow_status.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 overflow_status_cmd_fn() {
-  node -pe 'JSON.parse(process.argv[1]).status' "$(curl -s 127.0.0.1:3000/circuit/3)"
+  _status=(`node -pe 'JSON.parse(process.argv[1]).status' "$(curl -s 127.0.0.1:3000/circuit/3)"`)
+  if [[ ${_status} == 0 ]]; then
+    echo ""
+  else
+    echo "1"
+  fi
 }
 overflow_status_cmd_fn
--- poolpi/vacuum/vac_status.sh
+++ poolpi/vacuum/vac_status.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 vac_status_cmd_fn() {
-  node -pe 'JSON.parse(process.argv[1]).status' "$(curl -s 127.0.0.1:3000/circuit/2)"
+  _status=(`node -pe 'JSON.parse(process.argv[1]).status' "$(curl -s 127.0.0.1:3000/circuit/2)"`)
+  if [[ ${_status} == 0 ]]; then
+    echo ""
+  else
+    echo "1"
+  fi
 }
 vac_status_cmd_fn
