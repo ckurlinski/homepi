d15fd8338eac67151ee982a2331bfb9dcda68ff4
ckurlinski <ckurlinski@me.com>
2017-07-07 11:43:09 -0400

    Updated source includes

--- incl.sh
+++ incl.sh
@@ -7,14 +7,12 @@ source config/homepi.conf
 source config/poolpi.conf
 
 # Execute this:
+# find . -print | grep .sh | xargs chmod 644
+# to chmod to 644
+
+# Execute this:
 # find . -print | grep .sh | sed 's/\.\///g' | awk '{print"source "$0}'
 # to update the source Shell Script list
-source poolpi/circuits/overflow/overflow_off.sh
-source poolpi/circuits/overflow/overflow_on.sh
-source poolpi/circuits/overflow/overflow_status.sh
-source poolpi/circuits/vacuum/vac_off.sh
-source poolpi/circuits/vacuum/vac_on.sh
-source poolpi/circuits/vacuum/vac_status.sh
 source poolpi/poolcontroller_install.sh
 source poolpi/poolcontroller_service_install.sh
 source poolpi/src/circuit_select.sh
