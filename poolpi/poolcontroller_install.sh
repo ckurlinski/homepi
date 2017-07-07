ef3be4a36fddcd3e33c95d987c9a260fa6f886b4
ckurlinski <ckurlinski@me.com>
2017-07-07 16:24:18 -0400

    Updated missing source

--- poolpi/poolcontroller_install.sh
+++ poolpi/poolcontroller_install.sh
@@ -2,6 +2,7 @@
 
 source config/poolpi.conf
 source config/homebridge.conf
+source config/base.conf
 
 _pool_controller_install() {
   _install_git_software_fn
--- poolpi/poolcontroller_service_install.sh
+++ poolpi/poolcontroller_service_install.sh
@@ -2,6 +2,7 @@
 
 source config/poolpi.conf
 source config/homebridge.conf
+source config/base.conf
 
 _pool_controller_service_install() {
   _systemd_service_install_fn
