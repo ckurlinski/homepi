a8c012509e04cbe9bccdade6c0ead5dd929e64a9
ckurlinski <ckurlinski@me.com>
2017-07-07 15:40:55 -0400

    Goign back to (1) menu

--- systemd/systemd_services.sh
+++ systemd/systemd_services.sh
@@ -3,10 +3,7 @@
 # Select service
 # list is defined in config/base.conf
   _systemd_service_select() {
-    l0=(
-      "homebridge"
-      "poolcontroller"
-    )
+    l0=${_systemd_service_list[@]}
     list_template
     _selected_service=${_list_output}
   }
