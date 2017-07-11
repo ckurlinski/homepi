880c8ee73716f6ea11cc5fbf1129b3e466c763ab
ckurlinski <ckurlinski@me.com>
2017-07-11 16:27:37 -0400

    Debugging nodejs install

--- NodeJS/nodejs_install.sh
+++ NodeJS/nodejs_install.sh
@@ -4,11 +4,12 @@
 	_nodejs_install() {
 		## Download NodeJS and extract
 			_header "Downloading ${node_link}"
-			wget -q ${node_link}
+			cd ${g_node_dir}
+			sudo wget -q ${node_link}
 			_success "Downloaded ${node_link}"
 		## Extracting NodeJS to system dir
 			_header "Extracting ${node_file} to ${g_node_dir}"
-			tar xJf ${node_file} --strip=1
+			sudo tar xJf ${node_file} --strip=1
 			_success "${node_file} Extracted"
 		## remove tmp files / folders
 			_header "Removing tmp file : ${node_file}"
