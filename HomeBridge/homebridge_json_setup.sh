24773b690e58c6483aa01be600dff1ff6b521f15
ckurlinski <ckurlinski@me.com>
2017-07-11 14:27:07 -0400

    resolving var in configs

--- HomeBridge/homebridge_json_setup.sh
+++ HomeBridge/homebridge_json_setup.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 source HomeBridge/homebridge.conf
 #------------------------------------------------------------------------------#
+# base json config
+	hb_config_list=(
+		"{"
+		"\"bridge\" : {"
+		"\"username\" : \"${hb_user_id}\","
+		"\"name\" : \"${hb_node_name}\","
+		"\"pin\" : \"${hb_pin_code}\","
+		"\"port\" : \"${hb_random_port}\""
+		"},"
+		"\"platforms\" : ["
+		"{"
+		"\"port\" : \"${hb_server_port}\","
+		"\"restart\" : \"sudo systemctl restart ${hb_name}\","
+		"\"name\" : \"${hb_node_man}\","
+		"\"log\" : \"systemd\","
+		"\"platform\" : \"Server\""
+		"}"
+		"]"
+		"}"
+	)
+#------------------------------------------------------------------------------#
 # set systemd name
 	_hb_sysd_name() {
 		sysd_name="${hb_name}"
@@ -145,6 +166,8 @@ source HomeBridge/homebridge.conf
 			_hb_node_name
 		# HomeBridge Node Manufacturer
 			_hb_node_man
+		# Homebridge Device name
+			_hb_node_device_name
 		# Create Random Username
 			_hb_user_id
 		# Create random Port
