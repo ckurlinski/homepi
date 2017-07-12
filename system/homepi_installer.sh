3d4ff090e1bf492b09f685521ce4e1efe8b2b981
ckurlinski <ckurlinski@me.com>
2017-07-12 16:34:10 -0400

    testing installer

---
+++ system/homepi_installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#------------------------------------------------------------------------------#
+# Create symbolic link in ${g_node_dir} to homepi
+	_sym_link_homepi() {
+		if [[ -e "${g_node_dir}/bin/homepi" ]]; then
+			echo "Welcome to HomePi"
+		else
+			sudo ln -s $(pwd)/homepi ${g_node_dir}/bin/homepi
+				if [[ -e "${g_node_dir}/bin/homepi" ]]; then
+					sudo chmod 755 ${g_node_dir}/bin/homepi
+				fi
+			echo "Welcome to HomePi"
+		fi
+	}
