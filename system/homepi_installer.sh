8eb940a19d4ba74eea804d083e6f23f5789c18b9
ckurlinski <ckurlinski@me.com>
2017-07-12 16:51:36 -0400

    Added rm invalid links and add valid to system bin

--- system/homepi_installer.sh
+++ system/homepi_installer.sh
@@ -3,7 +3,15 @@
 # Create symbolic link in ${g_node_dir} to homepi
 	_sym_link_homepi() {
 		if [[ -e "${g_node_dir}/bin/homepi" ]]; then
-			echo "Welcome to HomePi"
+			if [[ -e $(readlink -f ${g_node_dir}/bin/homepi) ]]; then
+				echo "Welcome to HomePi"
+			else
+				sudo rm ${g_node_dir}/bin/homepi
+				sudo ln -s $(pwd)/homepi ${g_node_dir}/bin/homepi
+				if [[ -e "${g_node_dir}/bin/homepi" ]]; then
+					sudo chmod 755 ${g_node_dir}/bin/homepi
+				fi
+			fi
 		else
 			sudo ln -s $(pwd)/homepi ${g_node_dir}/bin/homepi
 				if [[ -e "${g_node_dir}/bin/homepi" ]]; then
