99a65d51c26bbf59b1327c8a05997cd98cc1c595
ckurlinski <ckurlinski@me.com>
2017-12-21 16:37:20 -0500

    update

--- system/homepi_installer.sh
+++ system/homepi_installer.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 #------------------------------------------------------------------------------#
+## _homepi_user_setup ##
+# HomePi User Setup
+	_homepi_user_setup() {
+        _user_name=${g_user}
+        _system_user_check
+        case ${_user_status} in
+            0) _success "User: ${_user_name} exists.....";;
+            1) _header "Create ${_user_name} - (y|N)?"
+				read _ans
+				case ${_ans} in
+					y) _system_user_create ;;
+					*) _warning "Aborting creating ${_user_name}....";;
+				esac ;;
+        esac
+    }
+#------------------------------------------------------------------------------#
+## _homepi_group_setup ##
+# HomePi Group Setup
+    _homepi_group_setup() {
+        _system_group_check
+		if [ $(id -nG ${_user_status} | grep -c ${_group_name}) == 0 ]; then
+			_error "${_user_status} is not a member of ${_group_name}"
+			_header "Adding ${_user_status} to the ${_group_name} group"
+				sudo usermod -G ${_group_name} ${_user_status}
+			_success "Added ${_user_status} to the ${_group_name} group"
+		else
+			_success "User ${_user_status} is already member of gpio group"
+		fi
+	}
+#------------------------------------------------------------------------------#
 # HomePi User folder Setup
 #------------------------------------------------------------------------------#
+## _homepi_install_folder_setup ##
 # HomePi install folder Setup
 	_homepi_install_folder_setup() {
 		_note "Setting npm global user:group to ${g_user}:${g_group}"
@@ -9,6 +40,7 @@
 		_success "Running scripts as ${g_user}:${g_group}"
 	}
 #------------------------------------------------------------------------------#
+## _sym_link_homepi ##
 # Create symbolic link in ${g_node_dir} to homepi
 	_sym_link_homepi() {
 		if [[ -e "${g_node_dir}/bin/homepi" ]]; then
@@ -33,3 +65,4 @@
 			echo "========================================"
 		fi
 	}
+#------------------------------------------------------------------------------#
